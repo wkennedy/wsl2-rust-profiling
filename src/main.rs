db0484cf0e3426b990069c2b2821c5ccb3756ec3
wkennedy <wildo.waggins@gmail.com>
2023-08-27 16:35:18 -0400

    Adding images, updated code example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::time::{Instant};
+
 fn main() {
-    println!("Hello, world!");
+    nested_loop_one();
+    single_loop();
+    nested_loop_two();
+}
+
+fn single_loop() {
+    for _n in 0..100 {
+        let instant = Instant::now();
+        let result = &format!("{}{:?}", "Time now: ", instant);
+        println!("{}", result);
+    }
+}
+
+fn nested_loop_one() {
+    for _n in 0..2000 {
+        for _i in 0..2000 {
+            let instant = Instant::now();
+            let result = &format!("{}{:?}", "Time now: ", instant);
+            println!("{}", result);
+        }
+    }
+}
+
+fn nested_loop_two() {
+    for _n in 0..2000 {
+        for _i in 0..2000 {
+            let instant = Instant::now();
+            let result = &format!("{}{:?}", "Time now: ", instant);
+            println!("{}", result);
+        }
+    }
 }
